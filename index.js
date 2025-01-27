5c7a8cb214f4681600f501ae7c69aeed64b171a5
homeira kalim <homeirakalim@gmail.com>
2025-01-27 13:04:26 -0500

    setting variables practice lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file!
+
+//the test expects companyName to be a constant equal to 'Scuber'
 const companyName = 'Scuber';
 
+//the test expects mostProfitableNeighborhood to be set to 'Chelsea' (no mention that it expects it to be a constant)
 let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
